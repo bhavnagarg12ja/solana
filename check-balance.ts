8a39561fcc9c294f76129dd4d8b9b9b51552a967
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-07-16 23:57:06 +0530

    minor fixes

--- check-balance.ts
+++ check-balance.ts
@@ -5,11 +5,19 @@ import {
   LAMPORTS_PER_SOL,
 } from '@solana/web3.js';
 
-const connection = new Connection(clusterApiUrl('devnet'));
-const address = new PublicKey('3j5HG166HRRGEXUPGcJMEJc7L1jeDqbSENFqD3DSnmaR');
-const balanceInLamports = await connection.getBalance(address);
-const balanceInSol = balanceInLamports / LAMPORTS_PER_SOL;
+async function main() {
+  const connection = new Connection(clusterApiUrl('devnet'));
+  const address = new PublicKey('3j5HG166HRRGEXUPGcJMEJc7L1jeDqbSENFqD3DSnmaR');
+  const balanceInLamports = await connection.getBalance(address);
+  const balanceInSol = balanceInLamports / LAMPORTS_PER_SOL;
 
-console.log(
-  `💰 Finished! The balance for the wallet at address ${address} is ${balanceInSol} SOL`
-);
+  console.log(
+    `💰 Finished! The balance for the wallet at address ${address} is ${balanceInSol} SOL`
+  );
+
+  console.log(`✅ Finished`);
+}
+
+main().catch((error) => {
+  console.error(error);
+});
--- example.ts
+++ example.ts
@@ -1,8 +1,14 @@
 import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
 
-const connection = new Connection(clusterApiUrl('devnet'));
-const address = new PublicKey('CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN');
-const balance = await connection.getBalance(address);
+async function main() {
+  const connection = new Connection(clusterApiUrl('devnet'));
+  const address = new PublicKey('CenYq6bDRB7p73EjsPEpiYN7uveyPUTdXkDkgUduboaN');
+  const balance = await connection.getBalance(address);
 
-console.log(`The balance of an account at ${address} is ${balance} SOL`);
-console.log(`✅ FInished`);
+  console.log(`The balance of an account at ${address} is ${balance} SOL`);
+  console.log(`✅ Finished`);
+}
+
+main().catch((error) => {
+  console.error(error);
+});
